9f702a53d9cd28ad897415f0a986902dfa5255fd
Siddhesh2811 <wanisiddhesh28@gmail.com>
2025-02-28 23:57:29 +0530

    demo commit

---
+++ my-app/src/components/customui/appBar/appBar.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle
+} from "@/components/ui/navigation-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import Link from 'next/link'
+import styles from '@/components/customui/appBar/appBar.module.css'
+import { Separator } from '@/components/ui/separator'
+
+const appBar = () => {
+    return (<div>
+        <div className={styles.container}>
+            <div className={styles.title}>
+                RILSSLLabs
+            </div>
+            <div className={styles.appBar}>
+                <NavigationMenu>
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                        <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Documentation
+            </NavigationMenuLink>
+          </Link>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <Link href='/certificate'>
+                                <NavigationMenuTrigger>Certificate</NavigationMenuTrigger></Link>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <Link href='/reports'>
+                                <NavigationMenuTrigger>Reports</NavigationMenuTrigger></Link>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <div className={styles.userIcon}>
+                                <Avatar className='w-5 h-5'>
+                                    <AvatarImage src="https://img.icons8.com/?size=100&id=84020&format=png&color=000000" />
+                                </Avatar></div>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            </div>
+            
+        </div>
+        <Separator />
+        </div>
+    )
+}
+
+export default appBar
